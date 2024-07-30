6a2fe00858969b03b18043863217d593017cd2e3
Nathalia Irokawa <nathalia.iro.fernandes@gmail.com>
2024-07-29 22:27:44 -0300

    Added animation for opening and closing of hamburger menu

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const menuToggleBackground = document.querySelector('.menu-toggle__background');
 
 menuToggle.addEventListener('click', () => {
     const isOpened = menuToggle.getAttribute('aria-expanded') === 'true';
-    console.log(isOpened);
     isOpened ? closeMenu() : openMenu();
 })
 
@@ -18,13 +17,22 @@ function openMenu() {
     iconToggle.setAttribute('src', 'images/icon-close-menu.svg');
     navHamburger.setAttribute('data-state', 'opened');
     menuToggleBackground.setAttribute('style', 'display: block');
+    
+    gsap.fromTo(navHamburger, {duration: 0.2, x: '100%', ease: 'power3.out'}, {x: 0});
+    gsap.from('.primary-item', {duration: 0.5, y: '-2rem', opacity: 0, stagger: 0.15, ease: 'power2.out'});
 }
 
 function closeMenu() {
     menuToggle.setAttribute('aria-expanded', 'false');
     iconToggle.setAttribute('src', 'images/icon-menu.svg');
-    navHamburger.setAttribute('data-state', 'closed');
-    menuToggleBackground.setAttribute('style', 'display: none');
+    
+    gsap.to(navHamburger, {duration: 0.3, x: '100%', ease: 'power2.out'});
+    gsap.to(menuToggleBackground, {duration: 0.3, opacity: 0});
+    
+    setTimeout(() => {
+        menuToggleBackground.setAttribute('style', 'display: none');
+        navHamburger.setAttribute('data-state', 'closed');
+    }, 300)
 }
 
 // ---- Dropdown Menu ----
